c5c40f2d66bc5ca6e5adca606440e51313a19a07
Ysaline-Degols-Vinci <128386131+Ysaline-Degols-Vinci@users.noreply.github.com>
2025-06-12 11:27:50 +0200

    basic drag and drop system

---
+++ Assets/Scripts/Inventory/InventoryItem.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class InventoryItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public Image image;
+    [HideInInspector] public Transform parentAfterDrag;
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        image.raycastTarget = false; 
+        parentAfterDrag = transform.parent; 
+        transform.SetParent(transform.root);
+        transform.SetAsLastSibling();
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        image.raycastTarget = true; 
+        transform.SetParent(parentAfterDrag);
+        transform.localPosition = Vector3.zero;
+
+    }
+}
